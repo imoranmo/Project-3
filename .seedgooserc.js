6973d1bee1b5a88c382ae5f9f3088db2f3a09a10
Luke Cole <Lukeajcole@gmail.com>
2021-11-13 10:09:08 -0800

    profile push 2

---
+++ .seedgooserc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    modelBaseDirectory: '/server/models', // model directory name
+    models: '*.js', // model matcher
+    data: './server/seeders', // data directory name
+    db: 'mongodb://localhost/alottarhythms' // db connection url
+  };
