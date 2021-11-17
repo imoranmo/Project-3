a843a89e90733c4e103b93fada6e7ae78e17a509
oliversh99 <oliv.code999@gmail.com>
2021-11-16 21:27:54 -0800

    git update from main

---
+++ client/src/components/Socialsignup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./socialsignup.css"
+// import { useQuery } from '@apollo/client';
+
+function Socialsignup(){
+return(
+    <div>
+ <div class="small-12 medium-6 column small-order-1 medium-order-2 login-box-social-section">
+ <div class="login-box-social-section-inner">
+   <span class="login-box-social-headline">Sign in with<br />your social network</span>
+   <a class="login-box-social-button-facebook">Log in with facebook</a>
+   <a class="login-box-social-button-twitter">Log in with Twitter</a>
+   <a class="login-box-social-button-google">Log in with Google+</a>
+ </div>
+</div>
+</div>
+)
+}
+export default Socialsignup;
