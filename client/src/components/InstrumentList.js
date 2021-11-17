dc91fe1d32c0db869dc629e62e682678abdcd0f4
Luke Cole <Lukeajcole@gmail.com>
2021-11-16 19:28:14 -0800

    big fux

--- client/src/components/InstrumentList.js
+++ client/src/components/InstrumentList.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { QUERY_INSTRUMENTS } from '../utils/queries';
 import { useQuery } from '@apollo/client';
+import { MultiSelect } from "react-multi-select-component";
 
 const InstrumentList = () => {
 
+    const [selected, setSelected] = useState([]);
     const { loading, data } = useQuery(QUERY_INSTRUMENTS);
 
 
   return (
 <>
-    <div class="flex items-center space-x-3 font-bold text-gray-700 text-2xl leading-8 mb-4">
-        <span>Instruments</span>
-    </div>
-    <div class="bg-white p-1 rounded-lg shadow-lg">
-        <div class="flex items-center space-x-3 font-semibold text-gray-900 text-2xl leading-8 mb-4 underline">
-        </div>
-        <div class="grid grid-cols-3 gap-1 mb-2">
-            <div class="rounded-lg bg-yellow-500 text-center">Banjo</div>
-        </div>
-    </div>
+
+<MultiSelect> 
+        type="date"
+        options={instrumentData.instruments}
+        name="instruments"
+        value={selected}
+        onChange={setSelected}
+    <MultiSelect/>
 </>
   )};
 
