557143c5e626afbc0ac871c8de06fa298ec868b0
Luke Cole <Lukeajcole@gmail.com>
2021-11-20 09:19:06 -0800

    heroku buildpack

--- server/.seedgooserc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    modelBaseDirectory: 'models', // model directory name
-    models: '*.js', // model matcher
-    data: './seeders', // data directory name
-    db: 'mongodb://localhost/alottarhythms' // db connection url
-  };
