59c890ce96c37b8e337f32a931961aed67103c34
Tina Ly <t.tinaly@hotmail.com>
2021-11-20 07:11:55 -0800

    styling, adding react image slide

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import RhythmFilter from '../components/RhythmFilter';
+import RhythmFilter from '../components/Slideshow';
 import Feed from '../components/Feed';
 
 
 
 import Auth from '../utils/auth';
+import Slideshow from '../components/Slideshow';
 
 const Home = () => {
     const loading = false
@@ -15,20 +16,20 @@ const Home = () => {
       }
 
   return (
-    <main className="w-screen">
+    <main className="w-screen bg-gray-100">
           {loading ? (
             <div>Loading...</div>
           ) : (
 <>
 <div className="flex justify-center w-screen">
-  <div className="w-full xl:w-1/2 w-screen mt-20 mb-6 ml-6 mr-10">
+  <div className="w-full xl:w-1/2 w-screen mt-20 mb-6 mr-10">
   <div className="p-8 flex items-center justify-between ">
         <h1 className="mb-4 text-3xl font-extrabold text-gray-700">Posts</h1>
-        <a href="/Post/new" className="rounded-lg border-2 bg-black border-black mr-6 text-white hover:bg-blue-600 hover:border-blue-600 p-2 font-medium">CREATE NEW POST</a>   
+        <a href="/Post/new" className="rounded-lg border-2 bg-black border-black mr-6 text-white hover:bg-blue-600 hover:border-blue-600 p-2 font-medium shadow-md">CREATE NEW POST</a>   
     </div>
     <Feed />
   </div>
-  <RhythmFilter />
+  <Slideshow />
 </div>
         
 </>
